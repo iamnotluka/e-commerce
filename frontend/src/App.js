2d3e524a84ed5082dd9f315d4b8b06d8649b2d2a
Luka Zoric <zoric.l@yahoo.com>
2020-12-02 17:53:57 +1000

    adding product data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import data from "./data";
 
 function App() {
 	const [open, setOpen] = useState(false);
@@ -39,66 +39,28 @@ function App() {
 			<main className="main">
 				<div className="content">
 					<ul className="products">
-						<li className="product">
-							<img
-								className="product-image"
-								src="https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain"
-								alt="Product"
-							/>
-							<div className="product-name">
-								<a href="product.html">Slim shirt</a>
-							</div>
-							<div className="product-brand">Nike</div>
-							<div className="product-price">$60</div>
-							<div className="product-rating">
-								4.5 Stars (10 Reviews)
-							</div>
-						</li>
-						<li className="product">
-							<img
-								className="product-image"
-								src="https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain"
-								alt="Product"
-							/>
-							<div className="product-name">
-								<a href="product.html">Slim shirt</a>
-							</div>
-							<div className="product-brand">Nike</div>
-							<div className="product-price">$60</div>
-							<div className="product-rating">
-								4.5 Stars (10 Reviews)
-							</div>
-						</li>
-						<li className="product">
-							<img
-								className="product-image"
-								src="https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain"
-								alt="Product"
-							/>
-							<div className="product-name">
-								<a href="product.html">Slim shirt</a>
-							</div>
-							<div className="product-brand">Nike</div>
-							<div className="product-price">$60</div>
-							<div className="product-rating">
-								4.5 Stars (10 Reviews)
-							</div>
-						</li>
-						<li className="product">
-							<img
-								className="product-image"
-								src="https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain"
-								alt="Product"
-							/>
-							<div className="product-name">
-								<a href="product.html">Slim shirt</a>
-							</div>
-							<div className="product-brand">Nike</div>
-							<div className="product-price">$60</div>
-							<div className="product-rating">
-								4.5 Stars (10 Reviews)
-							</div>
-						</li>
+						{data.products.map((product) => (
+							<li className="product">
+								<img
+									className="product-image"
+									src={product.image}
+									alt="Product"
+								/>
+								<div className="product-name">
+									<a href="product.html">{product.name}</a>
+								</div>
+								<div className="product-brand">
+									{product.brand}
+								</div>
+								<div className="product-price">
+									${product.price}
+								</div>
+								<div className="product-rating">
+									{product.rating} Stars ({product.numReviews}{" "}
+									Reviews)
+								</div>
+							</li>
+						))}
 					</ul>
 				</div>
 			</main>
---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+	products: [
+		{
+			name: "Slim Shirt",
+			category: "Shirts",
+			image:
+				"https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain",
+			price: 60,
+			brand: "Nike",
+			rating: 4.5,
+			numReviews: 10,
+		},
+		{
+			name: "Slim Shirt",
+			category: "Pants",
+			image:
+				"https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain",
+			price: 60,
+			brand: "Nike",
+			rating: 4.5,
+			numReviews: 10,
+		},
+		{
+			name: "Slim Pants",
+			category: "Pants",
+			image:
+				"https://images.asos-media.com/products/nike-football-dry-academy-t-shirt-in-black/11707330-1-black?$XXL$&wid=513&fit=constrain",
+			price: 60,
+			brand: "Nike",
+			rating: 4.5,
+			numReviews: 10,
+		},
+	],
+};
