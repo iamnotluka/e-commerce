ac7ad2f49f4307f63d9ba769b7cc1c7d6e18c767
Luka Zoric <zoric.l@yahoo.com>
2020-12-07 11:45:43 +1000

    fixed api issue

--- backend/server.js
+++ backend/server.js
@@ -13,29 +13,29 @@ const app = express();
 const port = 4321;
 
 mongoose
-  .connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .catch((error) => console.log(error.reason));
+	.connect(mongodbUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.catch((error) => console.log(error.reason));
 
-app.use("api/users", userRoute);
+app.use("/api/users", userRoute);
 
 app.get("/api/products", (req, res) => {
-  res.send(data.products);
+	res.send(data.products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const product = data.products.find((x) => x._id === productId);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ msg: "Product not found." });
-  }
+	const productId = req.params.id;
+	const product = data.products.find((x) => x._id === productId);
+	if (product) {
+		res.send(product);
+	} else {
+		res.status(404).send({ msg: "Product not found." });
+	}
 });
 
 app.listen(port, () => {
-  console.log("App running. Server started at", port);
+	console.log("App running. Server started at", port);
 });
