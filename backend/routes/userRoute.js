a202ce1ea30da5f1ea26879128816948aeea2410
Luka Zoric <luka.zoric@virtualmgr.com>
2020-12-07 10:45:13 +1000

    api that's not working properly

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import User from "../models/userModel";
+
+const router = express.Router();
+
+router.get("/createadmin", async (req, res) => {
+  try {
+    const user = new User({
+      name: "Luka",
+      email: "luka.zoric@griffithuni.edu.au",
+      password: "1234",
+      isAdmin: true,
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (err) {
+    res.send({ msg: error.message });
+  }
+});
+
+export default router;
